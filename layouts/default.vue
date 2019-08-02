29131cfbed2c171dce6b35fc5226b3d00075ddd9
mo49 <k.mochizuki.0409@gmail.com>
2019-08-02 15:02:45 +0900

    make roughly outline

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+*{
+  box-sizing: border-box;
+  outline: none;
+  -webkit-overflow-scrolling: touch; // overflow-scrollのとき慣性スクロール有効
+}
+
+html {
+  font-family: $font-family;
+  font-feature-settings : "palt";
+  color: $font-color-base;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,101 @@
+const { resolve } = require('path');
+
+const baseName = 'baseName'
+const baseDesc = 'baseDesc'
+const subDir = 'brandname'
+const baseUrl = `https://ja.nuxtjs.org/${subDir}`
+const baseOgp = '/image/ogp'
+const appId = ''
+const twitterId = ''
+
+export default {
+  mode: 'universal',
+  env: {
+    baseName: baseName,
+    baseDesc: baseDesc,
+    baseUrl: baseUrl,
+    baseOgp: baseOgp,
+    twitterId: twitterId,
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#'
+    },
+    title: baseName || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' },
+      { hid: 'description', name: 'description', content: baseDesc },
+      // og
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: baseUrl },
+      { hid: 'og:title', property: 'og:title', content: baseName },
+      { hid: 'og:description', property: 'og:description', content: baseDesc },
+      { hid: 'og:image', property: 'og:image', content: `${baseUrl}${baseOgp}/OGP.png` },
+      { hid: 'fb:app_id', content: appId },
+      // twitter
+      { hid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:title', property: 'twitter:title', content: baseName },
+      { hid: 'twitter:description', property: 'twitter:description', content: baseDesc },
+      { hid: 'twitter:image', property: 'twitter:image', content: `${baseUrl}${baseOgp}/OGP.png` },
+      { hid: 'twitter:site', property: 'twitter:site', content: `@${twitterId}` },
+      { hid: 'twitter:creator', property: 'twitter:creator', content: `@${twitterId}` },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: `${baseUrl}/favicon.ico` }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'reset-css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  devModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/style-resources',
+  ],
+  styleResources: {
+    scss: [
+      '~/assets/sass/_variable.scss',
+      '~/assets/sass/_mixin.scss'
+    ]
+  },
+
+  router: {
+    base: process.env.NODE_ENV === 'dev' ? '/' : `/${subDir}/`
+  },
+  generate: {
+    dir: resolve(__dirname, './dist/' + subDir),
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
