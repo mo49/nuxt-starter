812bc9174ade83e486216f63cb87e589d02c8769
mo49 <k.mochizuki.0409@gmail.com>
2019-11-20 19:02:39 +0900

    prototype -> inject

--- plugins/isFromIpad.js
+++ plugins/isFromIpad.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-
-Vue.prototype.$isFromIpad = () => {
-  if(process.browser){
-    // iPadOS対応iPad判定
-    const isFromIpad =
-      navigator.platform == "iPad" ||
-      (navigator.platform == "MacIntel" &&
-      navigator.userAgent.indexOf("Safari") != -1 &&
-      navigator.userAgent.indexOf("Chrome") == -1 &&
-      navigator.standalone !== undefined)
-    return isFromIpad
-  }
+export default ({app}, inject) => {
+  inject('isFromIpad', () => {
+    if(process.browser){
+      // iPadOS対応iPad判定
+      const isFromIpad =
+        navigator.platform == "iPad" ||
+        (navigator.platform == "MacIntel" &&
+        navigator.userAgent.indexOf("Safari") != -1 &&
+        navigator.userAgent.indexOf("Chrome") == -1 &&
+        navigator.standalone !== undefined)
+      return isFromIpad
+    } else {
+      return false
+    }
+  })
 }
